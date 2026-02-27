210c3f866ea5e77cfc72e2f3b0fa5c777e0ed20a
Wixisiz <SennyLu101@gmail.com>
2026-02-27 16:10:49 -0500

    test

---
+++ Assets/TestExtension/Editor/NewMonoBehaviourScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+
+public class NewMonoBehaviourScript : EditorWindow
+{
+
+    [MenuItem("DidIPressIt/Project")]
+    public static void InitProjectSetupTool()
+    {
+        
+    }
+}
