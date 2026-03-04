10eaf25b2f42c64cc326db44aa15795db8d616c4
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-04 12:52:42 -0500

    Moved most of documentation to README.md

--- Assets/Plugins/Editor/TestPlugin.cs
+++ Assets/Plugins/Editor/TestPlugin.cs
@@ -1,6 +1,6 @@
 /*******************************************************
 * Script:      TestPlugin.cs
-* Author(s):   Nicholas Johnson
+* Author(s):   Nicholas Johnson (Add yourselves to this as you!)
 * 
 * Description:
 *    A example plugin meant to showcase how to create plugins
@@ -10,20 +10,9 @@
 using UnityEngine;
 using UnityEditor;
 
-// For Unity to recognize this file as a plugin, it must be in a folder named "Editor".
-// To use this plugin, you must:
-//    1.) Open this project (the DIPT directory) in Unity.
-//    2.) Go to Tools > DIPT > TestPlugin from the menu bar (the bar with File, Edit, Assets, etc).
-// Right now, it does not do anything.
-
-// Below is an XML comment, it will display in VS & VSCode when hovering over
-// a field/method. I will use these whenever creating a method that will be called
-// in another file.
-
 /// <summary>
 /// An example plugin.
 /// </summary>
-
 public class TestPlugin : EditorWindow
 {
     [MenuItem("Tools/DIPT/Project")]
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DIPT
 Did I Press That (WIP)
+
+# HOW TO USE:
+To use this plugin, you must:
+    1.) Open this project (the DIPT directory) in Unity.
+    2.) Go to Tools > DIPT > TestPlugin from the menu bar (the bar with File, Edit, Assets, etc).
+Right now, clicking TestPlugin does not do anything. Feel free to put some test code in there
+to verify it works (this may have to be in a Start() function).
+
+# IMPORTANT
+For Unity to recognize this file as a plugin, it must be in a folder named "Editor".
+
+Below is an XML comment, it will display in VS & VSCode when hovering over
+a field/method. I will use these whenever creating a method that will be called
+in another file.
+
+/// <summary>
+/// When you put this right before a class, method, or variable, it will be displayed
+/// when you hover over it in VS or VSCode. In this case, hovering over example would
+/// show this text.
+/// </summary>
+public class Example
+{
+...
+}
