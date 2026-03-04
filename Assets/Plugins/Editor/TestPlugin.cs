ec7bb201506348b33a7d5abc0a839b7de294c91b
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-04 10:35:07 -0500

    Added some documentation

--- Assets/Plugins/Editor/TestPlugin.cs
+++ Assets/Plugins/Editor/TestPlugin.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEditor;
 
+// For Unity to recognize this file as a plugin, it must be in a folder named "Editor".
+// To use this plugin, you must:
+//    1.) Open this project (the DIPT directory) in Unity.
+//    2.) Go to Tools > DIPT > TestPlugin from the menu bar (the bar with File, Edit, Assets, etc).
+// Right now, it does not do anything.
+
+// Below is an XML comment, it will display in VS & VSCode when hovering over
+// a field/method. I will use these whenever creating a method that will be called
+// in another file.
+
+/// <summary>
+/// An example plugin.
+/// </summary>
+
 public class TestPlugin : EditorWindow
 {
-
-    [MenuItem("DIPT/Project")]
+    [MenuItem("Tools/DIPT/Project")]
     public static void InitProjectSetupTool()
     {
         
