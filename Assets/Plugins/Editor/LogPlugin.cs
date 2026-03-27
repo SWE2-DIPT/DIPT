13f8107c61e45b6614a9b340ebcbc30a69ad5c75
Andrew Brandao <andrewbrandao88@gmail.com>
2026-03-27 13:33:47 -0400

    added d-pad and made triggers longer

--- Assets/Plugins/Editor/LogPlugin.cs
+++ Assets/Plugins/Editor/LogPlugin.cs
@@ -4,15 +4,23 @@ using System.Collections.Generic;
 
 public class LogPlugin : EditorWindow
 {
-    public enum InputState { Pressed, Released, Movement }
-    private InputState activeState = InputState.Pressed;
+    public enum InputState { All, Pressed, Released, Movement }
 
-    private readonly List<string> pressedLogs = new List<string>();
-    private readonly List<string> releasedLogs = new List<string>();
-    private readonly List<string> movementLogs = new List<string>();
+    private class LogEntry
+    {
+        public InputState state;
+        public string message;
 
-    private Vector2 scrollPosPressed, scrollPosReleased, scrollPosMovement;
+        public LogEntry(InputState state, string message)
+        {
+            this.state = state;
+            this.message = message;
+        }
+    }
 
+    private Vector2 scrollPos;
+    private InputState activeState = InputState.All;
+    private readonly List<LogEntry> allLogs = new List<LogEntry>();
     private ControllerComponents controllerReader;
     private bool isPolling = true;
 
@@ -53,26 +61,26 @@ public class LogPlugin : EditorWindow
         controllerReader.GetJoystickActivity();
         controllerReader.GetTriggerActivity();
         controllerReader.GetButtonActivity();
-    }   
+    }
 
     private void HandlePressedLog(string message)
     {
-        pressedLogs.Add(message);
-        scrollPosPressed.y = Mathf.Infinity;
+        allLogs.Add(new LogEntry(InputState.Pressed, message));
+        scrollPos.y = Mathf.Infinity;
         Repaint();
     }
 
     private void HandleReleasedLog(string message)
     {
-        releasedLogs.Add(message);
-        scrollPosReleased.y = Mathf.Infinity;
+        allLogs.Add(new LogEntry(InputState.Released, message));
+        scrollPos.y = Mathf.Infinity;
         Repaint();
     }
 
     private void HandleMovementLog(string message)
     {
-        movementLogs.Add(message);
-        scrollPosMovement.y = Mathf.Infinity;
+        allLogs.Add(new LogEntry(InputState.Movement, message));
+        scrollPos.y = Mathf.Infinity;
         Repaint();
     }
 
@@ -81,65 +89,119 @@ public class LogPlugin : EditorWindow
         EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
         GUILayout.Label("Filter:", GUILayout.Width(50));
 
-        string[] options = { "Show Pressed", "Show Released", "Show Movement" };
+        string[] options = { "Show All", "Show Pressed", "Show Released", "Show Movement" };
         activeState = (InputState)GUILayout.Toolbar((int)activeState, options, EditorStyles.toolbarButton);
-        EditorGUILayout.EndHorizontal();
 
+        EditorGUILayout.EndHorizontal();
         EditorGUILayout.Space(5);
 
         isPolling = EditorGUILayout.Toggle("Polling Enabled", isPolling);
 
         EditorGUILayout.Space(5);
 
-        switch (activeState)
+        DrawLogArea(ref scrollPos, allLogs, GetFilterTitle());
+
+        EditorGUILayout.Space(5);
+
+        EditorGUILayout.BeginHorizontal(); // DEBUG LINES
+
+        if (GUILayout.Button("Add Pressed Debug", GUILayout.Height(30))) // DEBUG LINES
         {
-            case InputState.Pressed:
-                DrawLogArea(ref scrollPosPressed, pressedLogs, "PRESSED LOGS");
-                break;
-            case InputState.Released:
-                DrawLogArea(ref scrollPosReleased, releasedLogs, "RELEASED LOGS");
-                break;
-            case InputState.Movement:
-                DrawLogArea(ref scrollPosMovement, movementLogs, "MOVEMENT LOGS");
-                break;
+            HandlePressedLog(GetDebugMessage(InputState.Pressed)); // DEBUG LINES
         }
+
+        if (GUILayout.Button("Add Released Debug", GUILayout.Height(30))) // DEBUG LINES
+        {
+            HandleReleasedLog(GetDebugMessage(InputState.Released)); // DEBUG LINES
+        }
+
+        if (GUILayout.Button("Add Movement Debug", GUILayout.Height(30))) // DEBUG LINES
+        {
+            HandleMovementLog(GetDebugMessage(InputState.Movement)); // DEBUG LINES
+        }
+
+        EditorGUILayout.EndHorizontal(); // DEBUG LINES
 
         EditorGUILayout.Space(5);
 
         EditorGUILayout.BeginHorizontal();
+
         if (GUILayout.Button("Clear Current", GUILayout.Height(30)))
             ClearCurrent();
 
         if (GUILayout.Button("Clear All", GUILayout.Height(30)))
         {
-            pressedLogs.Clear();
-            releasedLogs.Clear();
-            movementLogs.Clear();
+            allLogs.Clear();
             Repaint();
         }
+
         EditorGUILayout.EndHorizontal();
     }
 
-    private void DrawLogArea(ref Vector2 scrollPos, List<string> logs, string title)
+    private string GetFilterTitle()
+    {
+        switch (activeState)
+        {
+            case InputState.All:
+                return "ALL LOGS";
+            case InputState.Pressed:
+                return "PRESSED LOGS";
+            case InputState.Released:
+                return "RELEASED LOGS";
+            case InputState.Movement:
+                return "MOVEMENT LOGS";
+            default:
+                return "LOGS";
+        }
+    }
+
+    private string GetDebugMessage(InputState state) // DEBUG LINES
+    {
+        string timestamp = System.DateTime.Now.ToString("HH:mm:ss"); // DEBUG LINES
+
+        switch (state) // DEBUG LINES
+        {
+            case InputState.Pressed: // DEBUG LINES
+                return $"[{timestamp}] DEBUG pressed log"; // DEBUG LINES
+
+            case InputState.Released: // DEBUG LINES
+                return $"[{timestamp}] DEBUG released log"; // DEBUG LINES
+
+            case InputState.Movement: // DEBUG LINES
+                return $"[{timestamp}] DEBUG movement log"; // DEBUG LINES
+
+            default: // DEBUG LINES
+                return $"[{timestamp}] DEBUG log"; // DEBUG LINES
+        }
+    }
+
+    private void DrawLogArea(ref Vector2 scrollPos, List<LogEntry> logs, string title)
     {
         EditorGUILayout.LabelField(title, EditorStyles.boldLabel);
 
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.ExpandHeight(true));
 
         for (int i = 0; i < logs.Count; i++)
-            EditorGUILayout.LabelField(logs[i], EditorStyles.helpBox);
+        {
+            if (activeState == InputState.All || logs[i].state == activeState)
+            {
+                EditorGUILayout.LabelField(logs[i].message, EditorStyles.helpBox);
+            }
+        }
 
         EditorGUILayout.EndScrollView();
     }
 
     private void ClearCurrent()
     {
-        if (activeState == InputState.Pressed)
-            pressedLogs.Clear();
-        else if (activeState == InputState.Released)
-            releasedLogs.Clear();
+        if (activeState == InputState.All)
+        {
+            allLogs.Clear();
+        }
         else
-            movementLogs.Clear();
+        {
+            allLogs.RemoveAll(log => log.state == activeState);
+        }
 
         Repaint();
     }
