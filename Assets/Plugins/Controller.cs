dfa97515bb8ee999702099d96b8e9a25bb11c595
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-04-01 16:54:09 -0400

    Started development on the Controller.cs class

--- Assets/Plugins/Controller.cs
+++ Assets/Plugins/Controller.cs
@@ -1,4 +1,13 @@
-public class Controller
+/*******************************************************
+* Script:      Controller.cs
+* Author(s):   Nicholas Johnson (Add yourselves to this!)
+* 
+* Description:
+*    A controller object that is the centralized source of truth
+*    for input states.
+*******************************************************/
+
+public static class Controller
 {
-    
+    enum buttons { Y, B, A, X, Up, Down, Left, Right, LBumper, RBumper }
 }
