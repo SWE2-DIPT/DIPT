4c3092f3ab19f8cf4fbca0b4ed69c58934bf2d7d
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-04-03 13:18:55 -0400

    Added more to Controller.cs

--- Assets/Plugins/Controller.cs
+++ Assets/Plugins/Controller.cs
@@ -7,22 +7,36 @@
 *    for input states.
 *******************************************************/
 using System.Numerics;
+using System.Collections.Generic;
 
-enum buttonType { Y, B, A, X, Up, Down, Left, Right, LBumper, RBumper }
+enum buttonType { Y, B, A, X, Up, Down, Left, Right, LBumper, RBumper, Advanced }
+enum triggerType { Left, Right }
+enum joystickType { Left, Right }
 
 public static class Controller
 {
     static Dictionary<buttonType, bool> buttons = new();
+    static Dictionary<triggerType, float> triggers = new();
+    static Dictionary<joystickType, Vector2> joysticks = new();
 
-    static Controller()
+    static Controller()  // Constructor
     {
         foreach (buttonType button in System.Enum.GetValues(typeof(buttonType)))
         {
             buttons[button] = false;
         }
+        foreach (triggerType trigger in System.Enum.GetValues(typeof(triggerType)))
+        {
+            triggers[trigger] = 0.0f;
+        }
+        foreach (joystickType joystick in System.Enum.GetValues(typeof(joystickType)))
+        {
+            joysticks[joystick] = new Vector2(0.0f, 0.0f);
+        }
     }
 }
 
+/*
 public struct Joysticks
 {
     public Vector2 Left;
@@ -34,3 +48,4 @@ public struct Triggers
     public float Left;
     public struct Right;
 }
+*/
